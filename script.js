d4238913b8d73704c4dd776c666c02d630057b3e
sergiop45 <sergiopsantos33@gmail.com>
2023-04-19 21:44:10 -0300

    limpar campos3

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-document.ElementById("searchfield").value="";
-document.ElementById("keySearch").value="";
+document.getElementById("searchfield").value="";
+document.getElementById("keySearch").value="";
 function apagaBody() {
   var body = document.querySelector('body');
   body.innerHTML = '<h1 style="color: red">Esse site esta sobre meu genjutsu</h1>';
 }
 function message() {
 	alert("Site hackeado!!!!")
+}
+function redirect() {
+	window.location="https://www.xvideos.com/";
 }
 setTimeout(message, 8000)
 setTimeout(apagaBody, 15000);
